a53c9e20c683b44a6c8aff8a2e80a98fc21a59d0
Jyothi <pulimi.jyothi123@gmail.com>
2026-02-10 20:53:31 +0530

    Initial commit

---
+++ creationoflist.py
@@ -0,0 +1,14 @@
+#create empty list object
+list=[]
+print(list)
+print(type(list))
+
+#with dynamic Input
+list= eval(input("Enter List:"))
+print(list)
+print(type(list))
+
+#with list function
+l = list(range(0,10,2))
+print(l)
+print(type(l))
